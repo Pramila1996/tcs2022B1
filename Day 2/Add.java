4bb7e695cea105c69c25fa7aa52c2a7f4a6495a8
unknown <pramilagodekar@gmail.com>
2022-05-17 13:21:24 +0000

    My First java prgrm

---
+++ Day 2/Add.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20;
+		int b = 10;
+		int sum = a+b;
+		
+		System.out.println( "Addition of two numbers is "+sum);
+
+	}
+
+}
